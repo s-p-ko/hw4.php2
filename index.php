2232be82780fe170e674c214227aae26c61ef27f
Serhiy Kotsyuba <s-p-ko@ukr.net>
2019-04-04 15:11:55 +0300

    Some refactoring of the code

--- index.php
+++ index.php
@@ -4,10 +4,6 @@ require __DIR__ . '/autoload.php';
 $parts = explode('/', $_SERVER['REQUEST_URI']);
 $name = (!empty($parts[1])) ? ucfirst($parts[1]) : 'Index';
 
-//Переход к точке входа для админ-панели
-if ('Admin' == $name) {
-    header('Location: /admin');
-}
 $classController = '\App\Controllers\\' . $name;
 $controller = new $classController;
 $controller->data = (!empty($parts[2])) ? $parts[2] : null;
