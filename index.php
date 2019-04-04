1dcd16adf226e0cf5198df78f01623f17fb510f2
s-p-ko <s-p-ko@ukr.net>
2019-04-04 15:15:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require __DIR__ . '/autoload.php';
 
 $parts = explode('/', $_SERVER['REQUEST_URI']);
 $name = (!empty($parts[1])) ? ucfirst($parts[1]) : 'Index';
-
 $classController = '\App\Controllers\\' . $name;
 $controller = new $classController;
 $controller->data = (!empty($parts[2])) ? $parts[2] : null;
